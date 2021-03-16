2d2cdac28802062c9bfa8d59e39cb57bc23e8292
ISHITASHARMA09 <72793628+ISHITASHARMA09@users.noreply.github.com>
2021-03-16 17:43:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# hot-air-balloon
-whjr
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+var car1 , car2 , car3 , car4 ;
+var cars=[];
+
+
+function setup(){
+  canvas = createCanvas(displayWidth-20, displayHeight-30);
+  
+}
+
+
+
